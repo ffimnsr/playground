ef5c34bcad19825b432619652e0c8e2026e1fa4e
Edward Fitz Abucay <ffimnsr@gmail.com>
2025-01-08 23:38:04 +0800

    🍕 Add betterrest app
    Signed-off-by: Edward Fitz Abucay <ffimnsr@gmail.com>

---
+++ swift/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Edward Fitz Abucay on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
